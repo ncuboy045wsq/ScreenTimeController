bf406a233c90e12f7edfc8d6f05f7a49f872e9ba
Kang Kang2 Lin <linkang2@lenovo.com>
2021-08-11 18:10:35 +0800

    Init screen controller app

--- src/main/java/com/motorola/screentimecontroller/provider/ScreenUsageProviderEntry.java
+++
@@ -1,7 +0,0 @@
-package com.motorola.screentimecontroller.provider;
-
-public class ScreenUsageProviderEntry {
-    public static String AUTHORITY = "com.motorola.screenusagecontroller";
-    public static final String CONTENT_TYPE_SINGLE = "vnd.android.cursor.dir/";
-    public static final String CONTENT_TYPE_MULTI = "vnd.android.cursor.item/";
-}
