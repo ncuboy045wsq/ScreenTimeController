d0dae439a08bb14942e3d9346ab28dcffc8811ff
wusq8 <wusq8@lenovo.com>
2021-08-16 17:47:38 +0800

    在增加使用时长之前，先校验下keyguard密码

--- app/src/main/java/com/motorola/screentimecontroller/MotoAppScreenControlBlocker.java
+++ app/src/main/java/com/motorola/screentimecontroller/MotoAppScreenControlBlocker.java
@@ -2,16 +2,24 @@ package com.motorola.screentimecontroller;
 
 
 import android.app.Activity;
+import android.app.KeyguardManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.hardware.biometrics.BiometricPrompt;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.CancellationSignal;
+import android.os.Handler;
+import android.os.Looper;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.annotation.RequiresApi;
 
 import motorola.core_services.misc.MotoExtendManager;
 import motorola.core_services.screentimecontroller.bean.TaskBlockUpInfo;
@@ -20,43 +28,80 @@ import motorola.core_services.screentimecontroller.bean.TaskBlockUpInfo;
 public class MotoAppScreenControlBlocker extends Activity {
     private static final boolean DEBUG = true;
     private static final String TAG = "MotoAppScreenBlocker";
+    private static final int REQUEST_CODE_KEYGUARD = 1;
+    private KeyguardManager mKeyguardManager;
+    private Handler mHandler;
 
+    private EditText mEtInput;
+    private TaskBlockUpInfo mBlockUpInfo = null;
 
-    public static final String KEY_LIMITED_APP = "key_limited_app";
-    public static final String KEY_LIMITED_BLOCK_INFO = "key_limited_blockinfo";
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_blocker);
         final CharSequence appName = getIntent().getCharSequenceExtra("screen_control_app");
-        final TaskBlockUpInfo blockUpInfo = getIntent().getParcelableExtra("screen_control_policy");
+        mBlockUpInfo = getIntent().getParcelableExtra("screen_control_policy");
 
         TextView textView = (TextView) findViewById(R.id.tv_blocker_display);
         Button button = (Button) findViewById(R.id.btn_blocker_addtime);
 
-        final EditText input = (EditText) findViewById(R.id.et_blocker_input);
+        mEtInput = (EditText) findViewById(R.id.et_blocker_input);
 
         textView.setText(appName + " has been out of time, please add more time");
 
         button.setOnClickListener(v -> {
+            if (DEBUG) {
+                Log.d(TAG, "showKeyguardCredentialOfMainUser@1");
+            }
             try {
-                String time = input.getText().toString();
+                String time = mEtInput.getText().toString();
                 int timeInt = Integer.parseInt(time);
 
                 if (timeInt >= 24 * 60) {
                     Toast.makeText(this, "can't more than 24 hours", Toast.LENGTH_SHORT).show();
                     return;
                 }
-                blockUpInfo.setMaxUsage(blockUpInfo.getMaxUsage() + timeInt * 60 * 1000);
-                MotoExtendManager.getInstance(this).updateTaskBlockUpInfo(blockUpInfo);
-                finish();
             } catch (NumberFormatException e) {
                 Toast.makeText(this, "add fail", Toast.LENGTH_SHORT).show();
             }
+
+            MotoExtendManager.getInstance(this).showKeyguardCredentialOfMainUser(this, "I am title", () -> {
+                if (DEBUG) {
+                    addMaxTime();
+                }
+            });
         });
 
         IntentFilter filter = new IntentFilter();
         filter.addAction(Intent.ACTION_DATE_CHANGED);
         registerReceiver(mReceiver, filter);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE_KEYGUARD && resultCode == RESULT_OK) {
+            if (DEBUG) {
+                Log.d(TAG, "onActivityResult");
+            }
+            addMaxTime();
+        }
+    }
+
+    private void addMaxTime() {
+        try {
+            String time = mEtInput.getText().toString();
+            int timeInt = Integer.parseInt(time);
+
+            if (timeInt >= 24 * 60) {
+                Toast.makeText(this, "can't more than 24 hours", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            mBlockUpInfo.setMaxUsage(mBlockUpInfo.getMaxUsage() + timeInt * 60 * 1000);
+            MotoExtendManager.getInstance(this).updateTaskBlockUpInfo(mBlockUpInfo);
+            finish();
+        } catch (NumberFormatException e) {
+            Toast.makeText(this, "add fail", Toast.LENGTH_SHORT).show();
+        }
     }
 
     private BroadcastReceiver mReceiver = new BroadcastReceiver() {
